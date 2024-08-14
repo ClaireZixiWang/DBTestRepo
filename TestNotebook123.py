c1f69640e8b02154676b8b1e2df40024fa27ea50
ClaireZixiWang <ClaireZixiWang>
2024-08-14 04:18:13 +0000

    test adding some text

--- TestNotebook123.py
+++ TestNotebook123.py
@@ -1,2 +1,14 @@
 # Databricks notebook source
 print("hello world")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Example title
+# MAGIC ## Example subtitle
+# MAGIC
+# MAGIC This is some text.
+
+# COMMAND ----------
+
+
